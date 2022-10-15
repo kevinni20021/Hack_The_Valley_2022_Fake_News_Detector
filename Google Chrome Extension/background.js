31f676d695227050988e462f9596f8641ce816fa
Kevin Ni <kevin.kni.ni@gmail.com>
2022-10-15 18:20:22 -0400

    got the url

--- Google Chrome Extension/background.js
+++ Google Chrome Extension/background.js
@@ -4,7 +4,12 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete' && tab.active) {
         chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
             let url = tabs[0].url;
-            console.log(url);
+            console.log(url)
+            fetch('http://127.0.0.1:5000/scrape', {
+                method: 'POST',
+                body: JSON.stringify({url}),
+                })
+            }) .then((data) => console.log(data))
         });
     }
 })
--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-
+import requests
 from newspaper import Article
-from flask import Flask
-
+from flask import *
+from flask_cors import CORS, cross_origin
+from Detect import detect
+from Article_scraping import relatedArticles
 
 app = Flask(__name__)
-
-@app.route('/')
-def summerize(url):
-    article = Article(url)
-    article.download()
-    article.parse()
-    article.nlp()
-    return article.summary
+@app.route('/scrape', methods = ['POST', 'GET'])
+def summerize():
+    if request.method == 'GET':
+        return "ok", 200
+    if request.method == 'POST':
+        url = request.data.decode().split('"',4)[3]\
+        if not detect(url):
+            return []
+        article = Article(url)
+        article.download()
+        article.parse()
+        article.nlp()
+        print(article.summary)
+        return article.summary
 
 if __name__ == "__main__":
-    app.run
+    app.run(debug = True)
+    '''
+    #get url
+    url = "something"
+    if not detect(url):
+        pass;
+        #send not valid message back to frontend
+    else:
+        #if real news:
+            #send message back thats its real, along with summary, related articles and title
+    '''
+
+
+
+
