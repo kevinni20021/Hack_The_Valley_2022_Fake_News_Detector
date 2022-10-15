1ecc52b42bf9534b3ff9c854250a971c8514ae5d
Kevin Ni <kevin.kni.ni@gmail.com>
2022-10-15 05:39:17 -0400

    added some stuff

--- Google Chrome Extension/background.js
+++ Google Chrome Extension/background.js
@@ -1,1 +1,9 @@
 // Manifest V3
+
+chrome.runtime.onInstalled.addListenser(() => {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url;
+    });
+    
+});
+
