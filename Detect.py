e55da4026528f1188568724d522942b916e09fdd
Kevin Ni <kevin.kni.ni@gmail.com>
2022-10-15 20:05:43 -0400

    Merge branch 'main' of C:\Users\kevin\OneDrive\Desktop\Python\UTSC2022\Hack_The_Valley_2022_Fake_News_Detector with conflicts.

--- Detect.py
+++ Detect.py
@@ -30,7 +30,7 @@ def extractText(url: str) -> str:
     text = soup.get_text()
 
     # break into lines and remove leading and trailing space on each
-    lines = (line.strip() for line in text.splitlines())
+    lines = (line.strip() for line in text.splitlines() if len(line.strip())>20)
     # break multi-headlines into a line each
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
     # drop blank lines
